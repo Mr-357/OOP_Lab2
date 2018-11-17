16d2aefc5242236752e18fbbe13050dbc0e4bda9
Mihajlo <Mihajlo@DESKTOP-QV9P8RR>
2018-11-17 20:26:25 +0100

    more groundwork

--- Interfaces/IChecker.cs
+++ Interfaces/IChecker.cs
@@ -8,5 +8,25 @@ namespace Interfaces
 {
     interface IChecker
     {
+        bool Check(string toCheck, out String error);
+    }
+
+    public class Temperature : IChecker
+    {
+        public bool Check(string toCheck, out string error)
+        {
+            try
+            {
+                double check = double.Parse(toCheck);
+                // if(notInRange(check,temp?) return "not in range" false
+                error = null;
+                return true;
+            }
+            catch(Exception ex)
+            {
+                error = ex.Message;
+                return false;
+            }
+        }
     }
 }
